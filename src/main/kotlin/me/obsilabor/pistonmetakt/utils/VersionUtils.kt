4cb9a692332dea5578d6f14feb6070039858191d
mooz <63669478+mooziii@users.noreply.github.com>
2022-08-08 23:15:37 +0200

    Update VersionUtils.kt

--- src/main/kotlin/me/obsilabor/pistonmetakt/utils/VersionUtils.kt
+++ src/main/kotlin/me/obsilabor/pistonmetakt/utils/VersionUtils.kt
@@ -1,8 +1,14 @@
 package me.obsilabor.pistonmetakt.utils
 
+import me.obsilabor.pistonmetakt.PistonMetaClient
 import me.obsilabor.pistonmetakt.data.launchermeta.Version
 
-suspend operator fun Version.compareTo(other: io.ktor.http.content.Version): Int {
-    
+suspend operator fun Version.compareTo(other: Version): Int {
+    val versions = PistonMetaClient.getLauncherMeta().versions
+    return when(versions.indexOf(this).compareTo(versions.indexOf(other))) { // invert the result
+        -1 -> 1
+        1 -> -1
+        else -> 0
+    }
 }
 
